c2ee4b799e55822f67ea765046676457a53deaa4
vini-sr <vinicius123lee@gmail.com>
2026-08-27 20:27:25 -0300

    Delete L05.ex3.py

--- L05.ex3.py
+++
@@ -1,8 +0,0 @@
-def contar_multiplos_14(inicio, fim):
-    
-    primeiro = inicio if inicio % 14 == 0 else inicio + (14 - (inicio % 14))
-    
-    multiplos = list(range(primeiro, fim + 1, 14))
-    return len(multiplos)
-
-print(contar_multiplos_14(1067, 3627))
